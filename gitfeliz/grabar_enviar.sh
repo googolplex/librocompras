7ea2f03b1f378ef9c2f5d75ed8a2fc46a0ed8362
googolplex <googolplex@lcompras.biz>
2017-01-23 15:43:40 -0300

    primer commit

--- gitfeliz/grabar_enviar.sh
+++ gitfeliz/grabar_enviar.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cp -R /home/xoldfusion/Downloads/openxava-5.6/workspace/librocomprasv3/*  librocompras/librocomprasv3
+cp -r /home/xoldfusion/Downloads/openxava-5.6/workspace/librocomprasv3  librocompras
 cp /home/xoldfusion/Downloads/openxava-5.6/tomcat/conf/*.xml librocompras/tomcatfeliz
 cp *.sh librocompras/gitfeliz
 cd librocompras
