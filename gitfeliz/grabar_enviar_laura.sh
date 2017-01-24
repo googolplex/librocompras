4815bed2eeaa86f8d3cbbad7d0ebfe43fcc39aab
Laura <lara.nirvana67@gmail.com>
2017-01-24 09:50:35 -0300

    dia dos - laura commit

--- gitfeliz/grabar_enviar_laura.sh
+++ gitfeliz/grabar_enviar_laura.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-cp -r /home/xoldfusion/Downloads/openxava-5.6/workspace/librocomprasv3  librocompras
-# cp /home/xoldfusion/Downloads/openxava-5.6/tomcat/conf/*.xml librocompras/tomcatfeliz
+cp -r /home/laurasalinas/Downloads/openxava-5.6/workspace/librocomprasv3  librocompras
+# cp /home/laurasalinas/Downloads/openxava-5.6/tomcat/conf/*.xml librocompras/tomcatfeliz
 cp *.sh librocompras/gitfeliz
 cd librocompras
 git add -A
--- librocomprasv3/src/biz/lcompras/model/Estado.java
+++ librocomprasv3/src/biz/lcompras/model/Estado.java
@@ -1,5 +1,36 @@
 package biz.lcompras.model;
 
-public class Estado {
+import java.util.*;
 
+import javax.persistence.*;
+
+import org.openxava.annotations.*;
+import org.openxava.util.*;
+
+@Entity
+@Table(name="ESTADO",
+uniqueConstraints={
+        @UniqueConstraint(name="EST_DESCRIPCION_DUPLICADA", columnNames={"DESCRIPCION"})        
+})
+public class Estado extends SuperClaseFeliz  {
+	
+	@Required
+	@Column(length=200,nullable=false,name="DESCRIPCION")	
+	private String descripcion ;	
+	
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion.toUpperCase().trim();
+	}
+
+	@PreUpdate
+	private void ultimoPaso() {
+			Date mifechora = new java.util.Date() ;
+			super.setModificadoPor(Users.getCurrent()) ;
+			super.setFchUltMod(mifechora)  ;
+	}		
+	
 }
