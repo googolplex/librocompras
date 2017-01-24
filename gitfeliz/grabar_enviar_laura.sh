e17d5c052bf269de5cdebb8d3c5c8a5871dc15ed
Laura <lara.nirvana67@gmail.com>
2017-01-24 09:45:38 -0300

    dia dos - laura commit

---
+++ gitfeliz/grabar_enviar_laura.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+cp -r /home/xoldfusion/Downloads/openxava-5.6/workspace/librocomprasv3  librocompras
+# cp /home/xoldfusion/Downloads/openxava-5.6/tomcat/conf/*.xml librocompras/tomcatfeliz
+cp *.sh librocompras/gitfeliz
+cd librocompras
+git add -A
+git commit -a -m "dia dos - laura commit"
+git push -u origin master
