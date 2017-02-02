837ef06af52c7be1b4b551689c4744bed3ab1ac0
Roger Armoa <googolplex@lcompras.biz>
2017-02-02 15:53:39 -0300

    vista y entity class para la impresion - falta depurar la vista ultimo periodo

--- gitfeliz/grabar_enviar_limones.sh
+++ gitfeliz/grabar_enviar_limones.sh
@@ -12,5 +12,5 @@ cp  amazon2017llave/*.sh librocompras/amazon2017llave
 rm *.*~
 cd librocompras
 git add -A
-git commit -a -m "impresion con el boton para jasperreport - aun no anda la accion"
+git commit -a -m "vista y entity class para la impresion - falta depurar la vista ultimo periodo"
 git push -u origin master
