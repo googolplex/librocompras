bdaaced4ebca9c2dd6b0cfafcbdee7f6a42f224b
Roger Armoa <googolplex@lcompras.biz>
2017-02-03 15:43:13 -0300

    solo lectura para librocompras controllers

--- amazon2017llave/deployfelizwar.sh
+++ amazon2017llave/deployfelizwar.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+sudo /etc/init.d/tomcat7 stop
+rm -R /var/lib/tomcat7/webapps/librocomprasv3
 cp *.war  /var/lib/tomcat7/webapps
 cp *.jar  /usr/share/tomcat7/lib
 sudo /etc/init.d/tomcat7 restart
