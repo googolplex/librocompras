b1f91c33325e88dd649f7d1acc873ca2d7aef7a3
Roger Armoa <googolplex@lcompras.biz>
2017-02-15 12:40:15 -0300

    jasperserver me consume todo el java heap

--- librocomprasv3/src/biz/lcompras/acciones/LibroComprasImpresionAction.java
+++ librocomprasv3/src/biz/lcompras/acciones/LibroComprasImpresionAction.java
@@ -1,18 +1,51 @@
 package biz.lcompras.acciones;
 
 
+import java.sql.*;
 import java.util.*;
 import org.openxava.actions.*;
+import org.openxava.util.*;
+
+import biz.lcompras.beansfelices.*;
 import net.sf.jasperreports.engine.*;
+import net.sf.jasperreports.engine.fill.*;
+import org.apache.commons.dbutils.BeanProcessor;
+
 
 public class LibroComprasImpresionAction  extends JasperReportBaseAction {
 
+
+	Connection con = null ;
+	
 	@Override
 	protected JRDataSource getDataSource() throws Exception {
 		// return new JREmptyDataSource();
 		// si le devuelves null se le devuelve la conexion JDBC, para usar el SQL dentro del reporte
 		// http://www.openxava.org/OpenXavaDoc/apidocs/org/openxava/actions/JasperReportBaseAction.html#getDataSource()
-
+		
+		// String table = MetaModel.get("InvoiceDetail").getMapping().getTable();
+		// porque no es una tabla
+		try {
+		con = DataSourceConnectionProvider.getByComponent("Periodos").getConnection();		
+		PreparedStatement ps = con.prepareStatement("select * from vista_librocompras k order by tipomov, lc_fecha");
+		ResultSet rs = ps.executeQuery();
+		BeanProcessor bp = new BeanProcessor();
+		ArrayList<VistaLibroComprasBean> list = new ArrayList<VistaLibroComprasBean>();		
+		while(rs.next()) {
+			list.add((VistaLibroComprasBean) bp.toBean(rs, VistaLibroComprasBean.class));
+		}
+        rs.close();
+        ps.close();
+        // solo para debug
+        
+        // for(VistaLibroComprasBean mb : list){
+        //    System.out.println(mb.getLc_numerofactura());
+        // }        
+        
+		} catch (Exception e ){
+			e.printStackTrace();
+		}
+		
 		return null;
 	}
 
@@ -30,14 +63,20 @@ public class LibroComprasImpresionAction  extends JasperReportBaseAction {
 	}
 
 	@Override
-	protected Map<String,String> getParameters() throws Exception {
-		Map<String,String> parametros = new HashMap<String,String>() ;
+	protected Map<String, Object> getParameters() throws Exception {
+		JRFileVirtualizer virtualizer = new JRFileVirtualizer(2, "/tmp");
+				
+		Map<String, Object> parametros = new HashMap<String, Object>() ;
+		parametros.put(JRParameter.REPORT_VIRTUALIZER, virtualizer);
+
+//		parametros.put(JRParameter.REPORT_CONNECTION, con );
+		
 		// yo creo que no se va a usar porque tengo la clase donde pongo los parametros
 		// parametros.put("p_yyyymm","201701");
 		// parametros.put("p_contribuyente","581069");
 		// parametros.put("REPORT_CONNECTION", "jdbc:postgresql://52.207.31.88:5432/manzanas?user=manzanas&password=googolplex124abc");
-		// return parametros ;
-		return null ;
+		//		return null ;
+		return parametros ;
 	}
 
 	
---
+++ librocomprasv3/src/biz/lcompras/beansfelices/VistaLibroComprasBean.java
@@ -0,0 +1,103 @@
+package biz.lcompras.beansfelices;
+
+public class VistaLibroComprasBean {
+	private Long  	q22 ;
+	private String contribuyente ;
+	private String fecha ;
+	private String proveedor;
+	private Double exento ;
+	private Double grav10 ;
+	private Double grav5;
+	private Double iva10 ;
+	private Double iva5;
+	private Double total ;
+	private String lc_periodo ;
+	private String lc_numerofactura ;
+	private Long tiva ;
+	private String tipomov ;
+	public Long getQ22() {
+		return q22;
+	}
+	public void setQ22(Long q22) {
+		this.q22 = q22;
+	}
+	public String getContribuyente() {
+		return contribuyente;
+	}
+	public void setContribuyente(String contribuyente) {
+		this.contribuyente = contribuyente;
+	}
+	public String getFecha() {
+		return fecha;
+	}
+	public void setFecha(String fecha) {
+		this.fecha = fecha;
+	}
+	public String getProveedor() {
+		return proveedor;
+	}
+	public void setProveedor(String proveedor) {
+		this.proveedor = proveedor;
+	}
+	public Double getExento() {
+		return exento;
+	}
+	public void setExento(Double exento) {
+		this.exento = exento;
+	}
+	public Double getGrav10() {
+		return grav10;
+	}
+	public void setGrav10(Double grav10) {
+		this.grav10 = grav10;
+	}
+	public Double getGrav5() {
+		return grav5;
+	}
+	public void setGrav5(Double grav5) {
+		this.grav5 = grav5;
+	}
+	public Double getIva10() {
+		return iva10;
+	}
+	public void setIva10(Double iva10) {
+		this.iva10 = iva10;
+	}
+	public Double getIva5() {
+		return iva5;
+	}
+	public void setIva5(Double iva5) {
+		this.iva5 = iva5;
+	}
+	public Double getTotal() {
+		return total;
+	}
+	public void setTotal(Double total) {
+		this.total = total;
+	}
+	public String getLc_periodo() {
+		return lc_periodo;
+	}
+	public void setLc_periodo(String lc_periodo) {
+		this.lc_periodo = lc_periodo;
+	}
+	public String getLc_numerofactura() {
+		return lc_numerofactura;
+	}
+	public void setLc_numerofactura(String lc_numerofactura) {
+		this.lc_numerofactura = lc_numerofactura;
+	}
+	public Long getTiva() {
+		return tiva;
+	}
+	public void setTiva(Long tiva) {
+		this.tiva = tiva;
+	}
+	public String getTipomov() {
+		return tipomov;
+	}
+	public void setTipomov(String tipomov) {
+		this.tipomov = tipomov;
+	}
+	
+}
