458880dc67255915ebe5085aeaa208fa99fc66b3
Roger Armoa <googolplex@lcompras.biz>
2017-04-17 10:47:37 -0400

    agrego la VPN feliz

--- amazon2017llave/vpn_feliz/paso4_generar_certificado_vpn2.sh
+++ amazon2017llave/vpn_feliz/paso4_generar_certificado_vpn2.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 ipsec pki --pub --in vpn-server-key.pem \
 --type rsa | ipsec pki --issue --lifetime 1825 \
---cacert server-root-ca.pem \
+--cacert contabilidad-root-ca.pem \
 --cakey server-root-key.pem \
 --dn "C=US, O=Contabilidad VPN Server, CN=contabilidad.lcompras.biz" \
 --san contabilidad.lcompras.biz \
